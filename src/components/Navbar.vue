14927769b4f4aca2bd95d3c3dfce9fc225aa10c9
qexo2108 <qexo2108@gmail.com>
2019-11-06 19:33:48 +0100

    Minor fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,7 @@
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route" @click="drawer = !drawer">
           <v-list-tile-action>
-            <v-icon class="white--text">{{ link.icon }}.  </v-icon>
+            <v-icon class="white--text">{{ link.icon }} </v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
